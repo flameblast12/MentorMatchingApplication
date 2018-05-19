50c73fcb3d3c0960ecc46e0e61acaa55867d2b49
심승언 <flameblast12@gmail.com>
2018-05-19 21:39:16 +0900

    로그인 액티비티 추가

--- app/src/main/java/com/example/student/mentormatchingapplication/LoginActivity.java
+++ app/src/main/java/com/example/student/mentormatchingapplication/LoginActivity.java
@@ -2,13 +2,26 @@ package com.example.student.mentormatchingapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button LogIn;
+    private Button SignUp;
+    private EditText InputID;
+    private EditText InputPW;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_screen);
+
+        LogIn = findViewById(R.id.LoginButton);
+        SignUp = findViewById(R.id.SignupButton);
+        InputID = findViewById(R.id.IDText);
+        InputPW = findViewById(R.id.PasswordText);
+
 
     }
 }
